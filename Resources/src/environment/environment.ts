8d961cd205e03b5cbb63826e8f2e597af5ac1a60
rugbyprof <terry.griffin@mwsu.edu>
2018-10-15 14:41:14 -0500

    Pushed from Terrys-MacBook-Pro

---
+++ Resources/src/environment/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  // fbAppId: 186848361921511,
+  googleWebClientId: '401245627689-ke4oci7bc8440anmpo4dms1od5keoome.apps.googleusercontent.com',
+  firebase: {
+    apiKey: "AIzaSyCdh5VaHpfZclbdkqWw88CvL5sZwUrRUJ8",
+    authDomain: "msu-location-logger.firebaseapp.com",
+    databaseURL: "https://msu-location-logger.firebaseio.com",
+    projectId: "msu-location-logger",
+    storageBucket: "msu-location-logger.appspot.com",
+    messagingSenderId: "823122992538"
+  }
+};
